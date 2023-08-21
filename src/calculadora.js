52481235a7aa17005202b803b7ff14a2bd6ce298
Douglas Jaeger <douglasjaeger@gmail.com>
2023-08-21 20:56:56 -0300

    Inclusão pasta testes

---
+++ src/calculadora.js
@@ -0,0 +1,3 @@
+const {sun} = require('../src/calculadora.js');
+
+console.log('2,2');
---
+++ test/calculadora.test.js
@@ -0,0 +1,14 @@
+function sun(a, b) {
+    return a + b;
+}
+function sub(a, b) {
+    return a + b;
+}
+function mult(a, b) {
+    return a + b;
+}
+function div(a, b) {
+    return a + b;
+}
+
+module.exports(sun);
